3ab3f2c645fedf79f24d1c886d18732dbe23e6c1
ST <susanna.tuomi@myy.haaga-helia.fi>
2023-09-11 10:03:00 +0300

    Osa 2

--- src/usersAndPosts.ts
+++ src/usersAndPosts.ts
@@ -1,7 +1,28 @@
 import User from './types/User';
 import Post from './types/Post';
+import { read } from 'fs';
 
 let users = require('../users.json') as User[];
 let posts = require('../posts.json') as Post[];
 
+
+
 // Part 2: write your logic here
+
+
+function fetchUserPosts(){
+    for (let user of users){
+        console.log(`${user.firstName} ${user.lastName}`)
+
+        for (let post of posts){
+            if(post.userId === user.id){
+                console.log(`- ${post.title}`)
+            }
+        }
+        console.log('\n')
+    }
+}
+        
+
+fetchUserPosts();
+
